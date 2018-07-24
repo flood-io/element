f128351788f2260d29b30f08ee4e2d40e090ee5d
Lachie Cox <lachiec@gmail.com>
2018-07-24 17:49:24 +1000

    WIP

---
+++ packages/cli/src/utils/ConsoleReporter.ts
@@ -0,0 +1,59 @@
+import {
+	IReporter,
+	TraceData,
+	TestEvent,
+	CompoundMeasurement,
+	MeasurementKind,
+} from '@flood/element/ReporterAPI'
+import { TestScriptError } from '@flood/element/TestScriptAPI'
+import { Logger } from 'winston'
+
+export class ConsoleReporter implements IReporter {
+	public responseCode: string
+	public stepName: string
+
+	constructor(private logger: Logger) {}
+
+	reset(step: string): void {}
+
+	addMeasurement(measurement: string, value: string | number, label?: string): void {
+		this.logger.debug(`> ${measurement} ${label} ${value}`)
+	}
+
+	addCompoundMeasurement(
+		measurement: MeasurementKind,
+		value: CompoundMeasurement,
+		label: string,
+	): void {
+		this.logger.debug(`> ${measurement} ${label} ${value}`)
+	}
+
+	addTrace(traceData: TraceData, label: string): void {}
+
+	async flushMeasurements(): Promise<void> {}
+
+	testLifecycle(stage: TestEvent, label: string): void {
+		switch (stage) {
+			case TestEvent.AfterStepAction:
+				this.logger.info(`---> ${label}()`)
+				break
+			case TestEvent.BeforeStep:
+				this.logger.info(`===> Step '${label}'`)
+				break
+			case TestEvent.AfterStep:
+				this.logger.info(`---> Step '${label}' finished`)
+				break
+			case TestEvent.StepSkipped:
+				this.logger.info(`---- Step '${label}'`)
+				break
+		}
+	}
+
+	testInternalError(message: string, err: Error): void {}
+	testAssertionError(err: TestScriptError): void {}
+	testStepError(err: TestScriptError): void {}
+
+	testScriptConsole(method: string, message?: any, ...optionalParams: any[]): void {
+		this.logger[method](message, ...optionalParams)
+	}
+}
--- packages/cli/src/utils/Logger.ts
+++ packages/cli/src/utils/Logger.ts
@@ -1,34 +1,29 @@
-import { createLogger, transports, format, Logger } from "winston";
-import { Format } from "logform";
-import * as strftime from "strftime";
+import { createLogger, transports, format, Logger } from 'winston'
+import { Format } from 'logform'
+import * as strftime from 'strftime'
 
 export default function(
-  level: string = "debug",
-  colourise: boolean = false,
-  prefix: string = "1"
+	level: string = 'debug',
+	colourise: boolean = false,
+	prefix: string = '1',
 ): Logger {
-  let formats: Format[] = [
-    format.label({ label: prefix.padStart(2, "0") }),
-    format.timestamp({ format: () => strftime("%Y-%m-%d %T,%L", new Date()) })
-  ];
+	let formats: Format[] = [
+		format.label({ label: prefix.padStart(2, '0') }),
+		format.timestamp({ format: () => strftime('%Y-%m-%d %T,%L', new Date()) }),
+	]
 
-  if (colourise) {
-    formats.push(format.colorize());
-  }
+	if (colourise) {
+		formats.push(format.colorize())
+	}
 
-  formats.push(
-    format.printf(
-      info => `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`
-    )
-  );
+	formats.push(
+		format.printf(info => `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`),
+	)
 
-  console.log(formats);
-  console.log(transports);
-
-  return createLogger({
-    level: level,
-    format: format.combine(...formats),
-    transports: [new transports.Console({ handleExceptions: true })]
-    // handleExceptions: true
-  });
+	return createLogger({
+		level: level,
+		format: format.combine(...formats),
+		transports: [new transports.Console({ handleExceptions: true })],
+		// handleExceptions: true
+	})
 }

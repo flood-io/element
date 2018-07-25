47a20ea712d3732e3220239f4600d5e756cbbd26
Lachie Cox <lachiec@gmail.com>
2018-07-25 10:30:41 +1000

    simplify logging for CLI

--- packages/cli/src/utils/Logger.ts
+++ packages/cli/src/utils/Logger.ts
@@ -1,24 +1,19 @@
 import { createLogger, transports, format, Logger } from 'winston'
 import { Format } from 'logform'
-import * as strftime from 'strftime'
+// import * as strftime from 'strftime'
 
-export default function(
-	level: string = 'debug',
-	colourise: boolean = false,
-	prefix: string = '1',
-): Logger {
+export default function(level: string = 'debug', colourise: boolean = false): Logger {
+	const startTime = Date.now()
 	let formats: Format[] = [
-		format.label({ label: prefix.padStart(2, '0') }),
-		format.timestamp({ format: () => strftime('%Y-%m-%d %T,%L', new Date()) }),
+		// format.label({ label: prefix.padStart(2, '0') }),
+		format.timestamp({ format: () => `+${Math.floor((Date.now() - startTime) / 1000)}s` }),
 	]
 
 	if (colourise) {
 		formats.push(format.colorize())
 	}
 
-	formats.push(
-		format.printf(info => `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`),
-	)
+	formats.push(format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`))
 
 	return createLogger({
 		level: level,

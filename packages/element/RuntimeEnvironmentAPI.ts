3b9ed496ccc40ba86cd0843908ed8a2cf97a15b5
Lachie Cox <lachiec@gmail.com>
2018-07-27 11:05:24 +1000

    merged index module into RuntimeEnvironmentAPI to make way for index.d.ts

--- packages/element/RuntimeEnvironmentAPI.ts
+++ packages/element/RuntimeEnvironmentAPI.ts
@@ -1,4 +1,15 @@
 import WorkRoot from './src/runtime-environment/WorkRoot'
 import { RuntimeEnvironment } from './src/types'
+import { runCommandLine, runUntilExit, ElementOptions } from './src/Element'
+import PuppeteerDriver from './src/driver/Puppeteer'
+import * as types from './src/types'
 
-export { WorkRoot, RuntimeEnvironment }
+export {
+	WorkRoot,
+	RuntimeEnvironment,
+	ElementOptions,
+	runCommandLine,
+	runUntilExit,
+	PuppeteerDriver,
+	types,
+}
--- packages/element/index.ts
+++
@@ -1,5 +0,0 @@
-import { runCommandLine, runUntilExit, ElementOptions } from './src/Element'
-import PuppeteerDriver from './src/driver/Puppeteer'
-import * as types from './src/types'
-
-export { ElementOptions, runCommandLine, runUntilExit, PuppeteerDriver, types }

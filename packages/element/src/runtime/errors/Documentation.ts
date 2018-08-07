3b85b6361deac0fbc5b8a0d724173ad5fe762dd2
Lachie Cox <lachiec@gmail.com>
2018-08-07 11:41:22 +0700

    add action to doco

--- packages/element/src/runtime/errors/Documentation.ts
+++ packages/element/src/runtime/errors/Documentation.ts
@@ -39,7 +39,7 @@ export function structuredErrorToDocumentedError(
 		return liftWithDoc(script, sErr, sErr.message, documentationNeeded, sErr.callContext)
 	}
 
-	// its a net error, delegate
+	// delegate out to the various type handlers
 	if (sErr.data._kind === 'net') {
 		return script.liftError(netError(<StructuredError<NetworkErrorData>>sErr))
 	}
@@ -68,7 +68,7 @@ function netError(err: StructuredError<NetworkErrorData>): DocumentedError {
 			`Unable to visit ${url}`,
 			`Element tried to visit The URL ${url} but it responded with status code ${
 				err.data.code
-			}. We expected a response code 200-299.`,
+			}. Element expected a response code 200-299.`,
 		)
 	}
 
@@ -80,12 +80,12 @@ function netError(err: StructuredError<NetworkErrorData>): DocumentedError {
 }
 
 function actionError(err: StructuredError<ActionErrorData>): DocumentedError {
-	const { kind } = err.data
+	const { kind, action } = err.data
 	if (kind === 'execution-context-destroyed') {
 		return DocumentedError.documented(
 			err,
-			'Unable to perform action, most likely due to page navigation.',
-			`We tried to perform an action on an element whose puppeteer ExecutionContext was destroyed. 
+			`Unable to perform ${action}, most likely due to page navigation.`,
+			`The test script tried to perform a ${action} action on an element whose puppeteer ExecutionContext was destroyed. 
 
 This can occur when the browser navigates to a new page, and we try to perform an action on an element located on the previous page.
 
@@ -101,45 +101,3 @@ Things to try to fix:
 		documentationNeeded,
 	)
 }
-
-// return new DocumentedError(
-// `Unable to ${action} ${target.toErrorString()} as it is no longer in the DOM`,
-// `The DOM node you tried to ${action} was no longer attached to the browser's DOM tree.
-
-// This may have been caused by:
-// - Changes to the DOM between the time you located the element and ${action}ed it.`,
-//
-
-// function visitError<T, U>(
-// err: Error,
-// browser: any,
-// key: string,
-// callCtx: string,
-// url: string,
-// options: NavigationOptions = {},
-// ): StructuredError<U> {
-// debug('visitError', err)
-// const sErr = StructuredError.cast<NetworkErrorData>(err)
-// if (sErr) {
-// const { kind, subKind } = sErr.data
-// if (kind === 'net' && subKind == 'not-resolved') {
-// return new StructuredError<U>(
-// `Unable to resolve DNS for ${url}`,
-// `Element tried to visit The URL ${url} but it didn't resolve in DNS. This may be due to TODO`,
-// callCtx,
-// err,
-// )
-// } else if (kind === 'http' && subKind == 'not-ok') {
-// return new DocumentedError(
-// `Unable to visit ${url}`,
-// `Element tried to visit The URL ${url} but it responded with status code ${
-// sErr.data.code
-// }. We expected a response code 200-299.`,
-// callCtx,
-// err,
-// )
-// }
-// }
-
-// return DocumentedError.wrapUnhandledError(err, `Unable to visit ${url}`, callCtx)
-// }

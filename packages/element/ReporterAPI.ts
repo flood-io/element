32e3c0b5c4bdb40de4662b848efa9f1a8d5ad6ff
Lachie Cox <lachiec@gmail.com>
2018-08-07 14:54:48 +0700

    use direct re-exports

--- packages/element/ReporterAPI.ts
+++ packages/element/ReporterAPI.ts
@@ -1,10 +1,8 @@
-import {
+export {
 	IReporter,
 	MeasurementKind,
 	TraceData,
 	TestEvent,
 	CompoundMeasurement,
 } from './src/Reporter'
-import { ConsoleReporter } from './src/reporter/Console'
-
-export { IReporter, MeasurementKind, TraceData, TestEvent, CompoundMeasurement, ConsoleReporter }
+export { ConsoleReporter } from './src/reporter/Console'
--- packages/element/RuntimeEnvironmentAPI.ts
+++ packages/element/RuntimeEnvironmentAPI.ts
@@ -1,15 +1,8 @@
+export { RuntimeEnvironment } from './src/types'
+export { runCommandLine, runUntilExit, ElementOptions } from './src/Element'
+
 import WorkRoot from './src/runtime-environment/WorkRoot'
-import { RuntimeEnvironment } from './src/types'
-import { runCommandLine, runUntilExit, ElementOptions } from './src/Element'
 import PuppeteerDriver from './src/driver/Puppeteer'
 import * as types from './src/types'
 
-export {
-	WorkRoot,
-	RuntimeEnvironment,
-	ElementOptions,
-	runCommandLine,
-	runUntilExit,
-	PuppeteerDriver,
-	types,
-}
+export { WorkRoot, PuppeteerDriver, types }

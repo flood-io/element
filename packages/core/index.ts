a3d7a58c6f91d2ebfac72997015ec524340ae838
Lachie Cox <lachiec@gmail.com>
2018-08-29 10:21:06 +1000

    export the necessary stuff from core

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -21,3 +21,19 @@ export { TestScriptError, ITestScript, TestScriptOptions } from './src/TestScrip
 
 import { expect } from './src/utils/Expect'
 export { expect }
+
+// DSL
+export { Until } from './src/page/Until'
+export { Condition } from './src/page/Condition'
+export { Device, MouseButtons, Key } from './src/page/Enums'
+export { By } from './src/page/By'
+export { Locator } from './src/page/types'
+export { ElementHandle } from './src/page/types'
+export { TestData, TestDataImpl } from './src/test-data/TestData'
+export { setup, DEFAULT_SETTINGS } from './src/runtime/Settings'
+
+import { FloodProcessEnv, nullFloodProcessEnv } from './src/runtime-environment/types'
+
+export const ENV: FloodProcessEnv = nullFloodProcessEnv
+export { Browser, Browser as Driver, Locatable, NullableLocatable } from './src/runtime/types'
+export { step } from './src/runtime/Step'

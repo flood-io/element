98412cdfeba461dc9355995d0e211de068d73041
Lachie Cox <lachiec@gmail.com>
2018-09-03 17:16:19 +1000

    add some more doco

--- packages/element/src/test-data/TestData.ts
+++ packages/element/src/test-data/TestData.ts
@@ -3,7 +3,7 @@ import { Feeder, FeedFilterFunction } from './Feeder'
 import { Loader } from './Loader'
 
 /**
- * Use this to load test data which will be iterated over with each iteration of your test.
+ * A `TestDataFactory` is available to be imported into your test script as `TestData`. Use this to load a <[TestDataSource]> which provides new test data to each iteration of your test.
  */
 export interface TestDataFactory {
 	/**
@@ -23,12 +23,45 @@ export interface TestDataFactory {
 }
 
 /**
- * TestDataSource is the instance returned by <[TestData]>'s methods.
+ * TestDataSource is the instance returned by <[TestDataFactory]>'s methods.
+ *
+ * Call TestDataSource's methods to configure your data source:
+ *
+ * ```typescript
+ * import { step, Browser, TestData, TestSettings } from '@flood/element'
+ * export const settings: TestSettings = {
+ *   loopCount: -1
+ * }
+ *
+ * interface Row {
+ *   username: string
+ *   userID: number
+ * }
+ * TestData.fromCSV<Row>('users.csv')
+ *   .circular(false) // Switch off circular data iteration.
+ *                    // By default, when the end of the data is reached, it wraps to the beginning.
+ *   .shuffle()       // Shuffle the data
+ *
+ * export default () => {
+ *    step('Step 1', (browser: Browser, row: Row) => {
+ *      // for each loop, a different line from user.csv will be available as `row`
+ *    })
+ * }
+ * ```
  */
 export class TestDataSource<T> {
+	/**
+	 * @internal
+	 */
 	public feeder: Feeder<T>
+	/**
+	 * @internal
+	 */
 	public instanceID: string
 
+	/**
+	 * @internal
+	 */
 	constructor(private loader: Loader<T>) {
 		this.feeder = new Feeder<T>()
 	}
@@ -49,8 +82,9 @@ export class TestDataSource<T> {
 	}
 
 	/**
-	 * Instructs the data feeder to repeat the data set when it reaches the end.
-	 * @param circular optional, pass `false` to disable
+	 * Instructs the data feeder to repeat the data set when it reaches the end. TestData is circular by default; use this to turn wrapping off.
+	 *
+	 * @param circular Default: true. Pass `false` to disable.
 	 */
 	public circular(circular = true): TestDataSource<T> {
 		this.feeder.circular(circular)
@@ -59,7 +93,7 @@ export class TestDataSource<T> {
 
 	/**
 	 * Shuffles the data set using the Fisher-Yates method. Use this to randomise the order of your data. This will always be applied after filtering.
-	 * @param shuffle optional, pass `false` to disable
+	 * @param shuffle Default: true. Pass `false` to disable.
 	 */
 	public shuffle(shuffle = true): TestDataSource<T> {
 		this.feeder.shuffle(shuffle)
@@ -84,28 +118,46 @@ export class TestDataSource<T> {
 		return this
 	}
 
+	/**
+	 * @internal
+	 */
 	public feed(): Option<T> {
 		if (!this.loader.isLoaded) throw new Error(`Test data has not been loaded yet!`)
 		return this.feeder.feed()
 	}
 
+	/**
+	 * @internal
+	 */
 	public peek(): Option<T> {
 		if (!this.loader.isLoaded) throw new Error(`Test data has not been loaded yet!`)
 		return this.feeder.peek()
 	}
 
+	/**
+	 * @internal
+	 */
 	public get size(): number {
 		return this.feeder.size
 	}
 
+	/**
+	 * @internal
+	 */
 	public get isComplete(): boolean {
 		return this.feeder.isComplete
 	}
 
+	/**
+	 * @internal
+	 */
 	public get isEmpty(): boolean {
 		return this.feeder.isEmpty
 	}
 
+	/**
+	 * @internal
+	 */
 	public get isStart(): boolean {
 		return this.feeder.isStart
 	}

23251a6f427b1f0a55ea19de4b0388f12e26a7ad
Lachie Cox <lachiec@gmail.com>
2018-09-05 09:06:32 +1000

    comment out debug

--- packages/element/src/runtime/test-observers/Errors.ts
+++ packages/element/src/runtime/test-observers/Errors.ts
@@ -5,8 +5,8 @@ import { NoOpTestObserver } from './Observer'
 import { structuredErrorToDocumentedError } from '../errors/Documentation'
 import { AnyErrorData } from '../errors/Types'
 
-import * as debugFactory from 'debug'
-const debug = debugFactory('element:test:errors')
+// import * as debugFactory from 'debug'
+// const debug = debugFactory('element:test:errors')
 
 export default class ErrorObserver extends NoOpTestObserver {
 	async onStepError(test: Test, step: Step, err: StructuredError<AnyErrorData>) {

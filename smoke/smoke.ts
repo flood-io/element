4b99fc7f95d0887f78bfe6f92e564252e90069c9
Lachie Cox <lachiec@gmail.com>
2018-09-10 08:47:49 +1000

    chalk it

--- smoke/smoke.ts
+++ smoke/smoke.ts
@@ -16,7 +16,7 @@ const failTests = globSync('*.fail.ts', { cwd: tests, absolute: true })
 async function runTest(testScript: string, expectPass: boolean): Promise<boolean> {
 	const shortName = basename(testScript)
 	console.log('')
-	console.log(`{yellow ============ {magenta running test {blue ${shortName}}} ===========}`)
+	console.log(chalk`{yellow ============ {magenta running test {blue ${shortName}}} ===========}`)
 	// console.log(process.env)
 	const proc = spawn('element', ['run', testScript, '--chrome', '--verbose'], {
 		stdio: ['inherit', 'pipe', 'inherit'],

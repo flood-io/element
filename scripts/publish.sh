2b8f6be41bf793965b5140c9a419bdfd0cde41f8
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2018-09-10 16:42:23 +0200

    Use yarn build instead of npm to resolve deps

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 root=$HERE/..
 
-yarn exec lerna version --force-publish --no-push --ignore-changes scripts/publish.sh prerelease 
+yarn exec lerna version --force-publish --no-push --ignore-changes scripts/publish.sh prerelease
 
 version=$(cat $root/lerna.json | jq .version)
 

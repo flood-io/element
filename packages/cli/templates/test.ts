c170321fd383c0353c1cadfbcb24aae265bba8bc
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2018-09-10 15:15:36 +0200

    Update template example

--- packages/cli/templates/test.ts
+++ packages/cli/templates/test.ts
@@ -1,5 +1,6 @@
-import { step, TestSettings, Until, By, MouseButtons, Device, Browser } from '@flood/element'
+import { step, TestSettings, Until, By, Device } from '@flood/element'
 import * as assert from 'assert'
+
 export const settings: TestSettings = {
 	device: Device.iPadLandscape,
 	userAgent: 'flood-chrome-test',
@@ -9,20 +10,19 @@ export const settings: TestSettings = {
 
 /**
  * <%= title %>
- * Version: 1.0
+ * @version 1.0
  */
 export default () => {
-	step('Test: Start', async (browser: Browser) => {
+	step('Test: Start', async browser => {
 		await browser.visit('<%= url %>')
-
-		assert(true, 'congratulations!')
 
 		// let buttonLocator = By.css('#my-button')
 		// await browser.wait(Until.elementIsVisible(buttonLocator))
-		//
-		// const button = await browser.find(buttonLocator)
+
+		// const button = await browser.findElement(buttonLocator)
+		// assert.ok(await button.isDisplayed(), 'Button is visible')
 		// await button.click()
-		//
-		// await browser.takeScreenshot()
+
+		await browser.takeScreenshot()
 	})
 }

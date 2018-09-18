48ba58ac59c2e7eafaac16c74b88d6e4bfae0635
Lachie Cox <lachiec@gmail.com>
2018-09-18 15:40:50 +1000

    debugging

--- scripts/ci/publish.sh
+++ scripts/ci/publish.sh
@@ -9,5 +9,8 @@ source $HERE/config.sh
 
 test_env_file=$HERE/test-env
 
+echo pwd
+pwd
+
 echo "~~~ tests passed, publishing"
 docker run --rm -e NPM_TOKEN -e GITHUB_TOKEN -e BUILDKITE_BRANCH -v $(pwd):/src --env-file $test_env_file $DOCKER_IMAGE make publish-ci

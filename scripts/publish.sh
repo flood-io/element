670775a0c3630f6751dd058d45d88bc28eda4519
Lachie Cox <lachiec@gmail.com>
2018-09-18 14:17:54 +1000

    perhaps positional

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -11,11 +11,11 @@ branch=${BUILDKITE_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
 case $branch in
   beta|feature/open-source-everything)
     echo publishing beta
-    yarn exec lerna -- version --force-publish --no-push --allow-branch beta --allow-branch feature/open-source-everything --preid beta --ignore-changes scripts/publish.sh prerelease
+    yarn exec lerna -- version prerelease --force-publish --no-push --allow-branch beta --allow-branch feature/open-source-everything --preid beta --ignore-changes scripts/publish.sh
     ;;
   master)
     echo publishing master
-    yarn exec lerna -- version --force-publish --no-push --allow-branch master --ignore-changes scripts/publish.sh patch
+    yarn exec lerna -- version patch --force-publish --no-push --allow-branch master --ignore-changes scripts/publish.sh
     ;;
   *)
     echo "branch is $branch which I won't publish"

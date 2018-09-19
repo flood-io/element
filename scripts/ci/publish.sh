0f3a085398014039b62a47e2ad6fd52be59bbc4b
Lachie Cox <lachiec@gmail.com>
2018-09-19 15:31:22 +1000

    try again

--- scripts/ci/publish.sh
+++ scripts/ci/publish.sh
@@ -14,4 +14,4 @@ pwd
 ls -la .
 
 echo "~~~ tests passed, publishing"
-docker run --rm -e NPM_TOKEN -e GITHUB_TOKEN -e BUILDKITE_BRANCH -e BUILDKITE_COMMIT -v /home/core/build:/build --env-file $test_env_file $DOCKER_IMAGE make publish-ci
+docker run --rm -e NPM_TOKEN -e GITHUB_TOKEN -e BUILDKITE_BRANCH -e BUILDKITE_COMMIT --env-file $test_env_file $DOCKER_IMAGE make publish-ci

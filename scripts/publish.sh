ce62694bed339ffd45fe7c500adb1cba0cc9f147
Lachie Cox <lachiec@gmail.com>
2018-09-27 11:29:11 +1000

    reorder to minimise the risk of errors needing manual intervention

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -53,6 +53,17 @@ if [[ ! -f $npmrc ]]; then
 	echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > $npmrc
 fi
 
+# NOTE that publishing to npm is both immutable and impossible to make transactional across multiple packages
+# so, the principle is to try to make our mistakes early
+#
+# Current order:
+# lerna version - bumps versions, git commit & tag
+# package build - make sure we can build
+# soft commit:
+# push to github - updated version & tags - fixing incorrect git is easier than fixing npm
+# hard commit:
+# npm publish - both packages should publish unless there's eg an intermitted network problem
+# do the brew publish - can only do this once packages have been published
 
 case $branch in
   beta|feature/open-source-everything)
@@ -68,18 +79,24 @@ case $branch in
     exit 0
 esac
 
-echo '--- publishing @flood/element'
+echo '--- building @flood/element'
 cd $root/packages/element
 ./scripts/build.sh
-npm publish --access public --tag $npm_tag dist
 
 echo '--- publishing @flood/element-cli'
 cd $root/packages/cli
 yarn build
-npm publish --access public --tag $npm_tag
 
 echo '--- pushing new tags'
 git push
+
+echo '--- building @flood/element'
+cd $root/packages/element
+npm publish --access public --tag $npm_tag dist
+
+echo '--- publishing @flood/element-cli'
+cd $root/packages/cli
+npm publish --access public --tag $npm_tag
 
 echo '--- publishing brew tap'
 cd $root

98d8e92d57b6a803a72fbde12808ecff63a59b22
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2019-06-18 15:15:06 -0500

    Revert browser settings after step

--- packages/element/src/runtime/Test.ts
+++ packages/element/src/runtime/Test.ts
@@ -186,6 +186,9 @@ export default class Test {
 	) {
 		let error: Error | null = null
 		await testObserver.beforeStep(this, step)
+
+		let originalBrowserSettings = { ...browser.settings }
+
 		try {
 			debug(`Run step: ${step.name}`) // ${step.fn.toString()}`)
 
@@ -193,6 +196,8 @@ export default class Test {
 			await step.fn.call(null, browser, testDataRecord)
 		} catch (err) {
 			error = err
+		} finally {
+			browser.settings = originalBrowserSettings
 		}
 
 		if (error !== null) {

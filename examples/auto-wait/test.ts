6815574d20c2a60effc0a1b8fe8a0d6060e10704
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2019-09-04 18:24:01 +0800

    fix(examples): added auto wait exmaple for challenge

---
+++ examples/auto-wait/test.ts
@@ -0,0 +1,64 @@
+import { step, By, TestSettings } from '@flood/element'
+
+export const settings: TestSettings = {
+	loopCount: 1,
+	userAgent: 'I AM ROBOT',
+
+	// Auto wait until elements are visible before acting
+	waitUntil: 'visible',
+}
+
+export default () => {
+	step('Open page and click start', async b => {
+		await b.visit('https://challenge.flood.io')
+		await b.click(By.css('#new_challenger > input.btn.btn-xl.btn-default'))
+		await b.takeScreenshot()
+	})
+
+	step('Challenge: Step 1', async b => {
+		await b.selectByValue(By.id('challenger_age'), '31')
+		await b.takeScreenshot()
+		await b.click('input.btn')
+	})
+
+	step('Challenge: Step 2', async b => {
+		let orderElements = await b.findElements(By.css('table tbody tr td:first-of-type label'))
+		let orderIDs = await Promise.all(orderElements.map(element => element.text()))
+		let largestOrder = orderIDs
+			.map(Number)
+			.sort((a, b) => a - b)
+			.reverse()[0]
+
+		// Fill in text field
+		await b.type(By.id('challenger_largest_order'), String(largestOrder))
+		await b.takeScreenshot()
+
+		// Click label with order ID
+		await b.click(By.visibleText(String(largestOrder)))
+		await b.takeScreenshot()
+
+		await b.click(By.css('input.btn'))
+		await b.takeScreenshot()
+	})
+
+	step('Challenge: Step 3', async b => {
+		await b.click('input.btn')
+		await b.takeScreenshot()
+	})
+
+	step('Challenge: Step 4', async b => {
+		let element = await b.findElement('span.token')
+		let token = await element.text()
+		await b.type(By.id('challenger_one_time_token'), token)
+		await b.takeScreenshot()
+		await b.click('input.btn')
+	})
+
+	step('Challenge: Step 5', async b => {
+		let element = await b.findElement('h2')
+		await b.takeScreenshot()
+		let completionText = await element.text()
+		if (completionText.trim() !== "You're Done!")
+			throw new Error(`Expected header to be "You're Done!", got "${completionText}"`)
+	})
+}

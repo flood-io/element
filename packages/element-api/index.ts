195a1563ea90522feb0f8043c888f48f579e1540
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2019-09-09 17:28:19 +0800

    refactor(internal): expose API explicitly intead of *

--- packages/element-api/index.ts
+++ packages/element-api/index.ts
@@ -1,58 +1,47 @@
-export * from '@flood/element/api'
-
 // // CLI / runtime
-// export {
-// 	IReporter,
-// 	MeasurementKind,
-// 	TraceData,
-// 	TestEvent,
-// 	CompoundMeasurement,
-// 	NetworkTraceData,
-// } from '@flood/element/dist/src/Reporter'
+export {
+	IReporter,
+	MeasurementKind,
+	TraceData,
+	TestEvent,
+	CompoundMeasurement,
+	NetworkTraceData,
+} from '@flood/element/src/Reporter'
 
-// // XYZ
+// XYZ
 
-// export { runCommandLine, runUntilExit, ElementOptions } from '@flood/element/dist/src/Element'
+export { runCommandLine, runUntilExit, ElementOptions } from '@flood/element/src/Element'
 
-// export { TestSettings, ResponseTiming } from '@flood/element/dist/src/runtime/Settings'
-// export {
-// 	RuntimeEnvironment,
-// 	FloodProcessEnv,
-// } from '@flood/element/dist/src/runtime-environment/types'
-// import WorkRoot from '@flood/element/dist/src/runtime-environment/WorkRoot'
-// export {
-// 	nullRuntimeEnvironment,
-// } from '@flood/element/dist/src/runtime-environment/NullRuntimeEnvironment'
-// import { launch, PuppeteerClient } from '@flood/element/dist/src/driver/Puppeteer'
+export { TestSettings, ResponseTiming } from '@flood/element/src/runtime/Settings'
+export { RuntimeEnvironment, FloodProcessEnv } from '@flood/element/src/runtime-environment/types'
+import WorkRoot from '@flood/element/src/runtime-environment/WorkRoot'
+export {
+	nullRuntimeEnvironment,
+} from '@flood/element/src/runtime-environment/NullRuntimeEnvironment'
+import { launch, PuppeteerClient } from '@flood/element/src/driver/Puppeteer'
 
-// export { TestCommander } from '@flood/element/dist/src/Runner'
+export { TestCommander } from '@flood/element/src/Runner'
 
-// export { WorkRoot, PuppeteerClient, launch }
+export { WorkRoot, PuppeteerClient, launch }
 
-// export { TestScriptError, ITestScript, TestScriptOptions } from '@flood/element/dist/src/TestScript'
-// export { EvaluatedScript } from '@flood/element/dist/src/runtime/EvaluatedScript'
+export { TestScriptError, ITestScript, TestScriptOptions } from '@flood/element/src/TestScript'
+export { EvaluatedScript } from '@flood/element/src/runtime/EvaluatedScript'
 
-// export { expect } from '@flood/element/dist/src/utils/Expect'
+export { expect } from '@flood/element/src/utils/Expect'
 
-// // Test observer integration
-// export { default as Test } from '@flood/element/dist/src/runtime/Test'
-// export { Step } from '@flood/element/dist/src/runtime/Step'
+// Test observer integration
+export { default as Test } from '@flood/element/src/runtime/Test'
+export { Step } from '@flood/element/src/runtime/Step'
 
-// export {
-// 	NoOpTestObserver,
-// 	TestObserver,
-// } from '@flood/element/dist/src/runtime/test-observers/Observer'
+export { NoOpTestObserver, TestObserver } from '@flood/element/src/runtime/test-observers/Observer'
 
-// export { Assertion } from '@flood/element/dist/src/runtime/Assertion'
-// export {
-// 	AssertionErrorData,
-// 	castStructuredError,
-// } from '@flood/element/dist/src/runtime/errors/Types'
+export { Assertion } from '@flood/element/src/runtime/Assertion'
+export { AssertionErrorData, castStructuredError } from '@flood/element/src/runtime/errors/Types'
 
-// export { IObjectTrace, NullObjectTrace } from '@flood/element/dist/src/utils/ObjectTrace'
+export { IObjectTrace, NullObjectTrace } from '@flood/element/src/utils/ObjectTrace'
 
-// export { StructuredError } from '@flood/element/dist/src/utils/StructuredError'
+export { StructuredError } from '@flood/element/src/utils/StructuredError'
 
-// export { default as NetworkRecorder } from '@flood/element/dist/src/network/Recorder'
-// export { Entry } from '@flood/element/dist/src/network/Protocol'
-// export { default as NetworkObserver } from '@flood/element/dist/src/runtime/Observer'
+export { default as NetworkRecorder } from '@flood/element/src/network/Recorder'
+export { Entry } from '@flood/element/src/network/Protocol'
+export { default as NetworkObserver } from '@flood/element/src/runtime/Observer'

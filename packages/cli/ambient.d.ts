f954c5ee352e3b4f7238bf7044bcd6a500362a12
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2019-09-10 16:35:33 +0800

    refactor(cli): adds missing types

--- packages/cli/ambient.d.ts
+++ packages/cli/ambient.d.ts
@@ -1,10 +1,4 @@
-declare module 'yeoman-environment'
-
-declare module 'find-root'
-
 declare module 'recast'
-
-declare module 'command-exists'
 
 declare module 'puppeteer/DeviceDescriptors'
 
@@ -14,10 +8,6 @@ declare module 'knuth-shuffle'
 
 declare module 'comment-parser'
 
-declare module 'boxen'
-
 declare module 'update-check'
-
-declare module 'ms'
 
 // declare module 'bluebird'
--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import * as findRoot from 'find-root'
+import findRoot from 'find-root'
 const root = findRoot(__dirname)
 import { main } from './src/element'
 main(root)

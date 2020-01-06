307426cf0f2534c63e00c682a8d3b904823cd3e1
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2020-01-06 18:47:16 +0800

    fix(compiler): use babel to transpile instead of Webpack
    This should simplify resolver design, and allow us to add checking as a separate process.

--- packages/compiler/src/index.ts
+++ packages/compiler/src/index.ts
@@ -1,44 +1,94 @@
-import webpack, { Configuration as WebpackConfig, Stats } from 'webpack'
+import { Configuration as WebpackConfig, Stats } from 'webpack'
 import { CompilerOptions, sys } from 'typescript'
 import { resolve, dirname, join } from 'path'
-import MemoryFileSystem from 'memory-fs'
+// import MemoryFileSystem from 'memory-fs'
 import WebpackBar from 'webpackbar'
 import findRoot from 'find-root'
 
+import { transformFileAsync } from '@babel/core'
+
+// import { Context, Script, compileFunction, createContext, runInContext } from 'vm'
+
 export interface CompilerOutput {
 	content: string
-	stats: Stats
+	stats?: CompilerStats
+}
+
+class CompilerStats {
+	hash?: string
+	startTime?: number
+	endTime?: number
+
+	/** Returns true if there were errors while compiling. */
+	hasErrors(): boolean {
+		return false
+	}
+	/** Returns true if there were warnings while compiling. */
+	hasWarnings(): boolean {
+		return false
+	}
+	/** Returns a formatted string of the compilation information (similar to CLI output). */
+	toString(options?: Stats.ToStringOptions): string {
+		return 'Compiled'
+	}
 }
 
 export class Compiler {
-	constructor(private sourceFile: string, private productionMode: boolean = false) {}
+	constructor(private sourceFile: string /* , private productionMode: boolean = false */) {}
 
 	public async emit(): Promise<CompilerOutput> {
-		let compiler = webpack(this.webpackConfig)
-
-		let fileSystem = new MemoryFileSystem()
-
-		compiler.outputFileSystem = fileSystem
-
-		return new Promise((yeah, nah) => {
-			compiler.run((err, stats) => {
-				// let formattedStats = stats.toString()
-
-				if (stats.hasErrors() || stats.hasWarnings()) {
-					return nah(
-						new Error(
-							stats.toString({
-								errorDetails: true,
-								warnings: true,
-							}),
-						),
-					)
-				} else {
-					// let output = compiler.outputFileSystem as MemoryFileSystem
-					yeah({ stats, content: fileSystem.data['bundle.js'].toString() })
-				}
-			})
+		let result = await transformFileAsync(resolve(this.sourceFile), {
+			comments: false,
+			highlightCode: true,
+			presets: [
+				'@babel/preset-typescript',
+				[
+					'@babel/preset-env',
+					{
+						targets: { node: 'current' },
+						useBuiltIns: false,
+					},
+				],
+			],
 		})
+
+		let content: string = ''
+		if (result != null) {
+			content = result.code ?? ''
+		}
+
+		let stats = new CompilerStats()
+
+		return {
+			content,
+			stats,
+		}
+
+		// let compiler = webpack(this.webpackConfig)
+
+		// let fileSystem = new MemoryFileSystem()
+
+		// compiler.outputFileSystem = fileSystem
+
+		// return new Promise((yeah, nah) => {
+		// 	compiler.run((err, stats) => {
+		// 		// let formattedStats = stats.toString()
+
+		// 		if (stats.hasErrors() || stats.hasWarnings()) {
+		// 			return nah(
+		// 				new Error(
+		// 					stats.toString({
+		// 						errorDetails: true,
+		// 						warnings: true,
+		// 					}),
+		// 				),
+		// 			)
+		// 		} else {
+		// 			// let output = compiler.outputFileSystem as MemoryFileSystem
+		// 			yeah({ stats, content: fileSystem.data['bundle.js'].toString() })
+		// 		}
+		// 	})
+		// })
 	}
 
 	get compilerOptions(): CompilerOptions {
@@ -55,11 +105,14 @@ export class Compiler {
 
 		return {
 			entry: this.sourceFile,
-			mode: this.productionMode ? 'production' : 'development',
+			// mode: this.productionMode ? 'production' : 'development',
+			mode: 'none',
 			devtool: 'cheap-module-eval-source-map',
 			output: {
 				path: '/',
 				filename: join('bundle.js'),
+				globalObject: 'this',
+				libraryTarget: 'umd',
 			},
 			resolve: {
 				extensions: ['.ts', '.js'],
@@ -90,7 +143,7 @@ export class Compiler {
 								configFile: this.configFilePath,
 								onlyCompileBundledFiles: true,
 								reportFiles: [this.sourceFile],
-								transpileOnly: this.productionMode,
+								transpileOnly: true,
 								compilerOptions: this.compilerOptions,
 							},
 						},

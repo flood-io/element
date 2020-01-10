4a6e8f2a518b73fe4b6d1c5ff1f8b5d6f421bc06
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2020-01-10 12:15:27 +0800

    feat(compiler): adds support for sourcemaps

--- packages/core/src/test-script/WebpackCompiler.ts
+++ packages/core/src/test-script/WebpackCompiler.ts
@@ -2,19 +2,29 @@ import { Compiler, CompilerOutput } from '@flood/element-compiler'
 import parseComments from 'comment-parser'
 import { ITestScript } from '../ITestScript'
 import { VMScript } from 'vm2'
-import { TestScriptOptions, TestScriptDefaultOptions } from '../TestScript'
+import { TestScriptOptions, TestScriptDefaultOptions, TestScriptError } from '../TestScript'
 import { readFileSync } from 'fs-extra'
+import { SourceUnmapper } from './SourceUnmapper'
 
 export default class WebpackCompiler implements ITestScript {
 	private vmScriptCache: VMScript
 	private result?: CompilerOutput
+	private sourceUnmapper: SourceUnmapper
 
-	constructor(public sourceFile: string, options: TestScriptOptions = TestScriptDefaultOptions) {}
+	constructor(public sourceFile: string, _options: TestScriptOptions = TestScriptDefaultOptions) {}
 
 	public async compile(): Promise<this> {
-		let compiler = new Compiler(this.sourceFile)
-		let output = await compiler.emit()
+		const compiler = new Compiler(this.sourceFile)
+		const output = await compiler.emit()
 		this.result = output
+
+		this.sourceUnmapper = await SourceUnmapper.init(
+			// this.originalSource,
+			output.content,
+			this.sourceFile,
+			this.sourceMap,
+		)
+
 		return this
 	}
 
@@ -35,7 +45,7 @@ export default class WebpackCompiler implements ITestScript {
 	}
 
 	get sourceMap() {
-		return ''
+		return this.result?.sourceMap ?? ''
 	}
 
 	public get testName(): string {
@@ -47,13 +57,37 @@ export default class WebpackCompiler implements ITestScript {
 	}
 
 	public get formattedErrorString(): string {
-		return this.result?.stats?.toString({ errors: true, warnings: true }) ?? ''
+		return this.result?.stats?.toString() ?? ''
 	}
 
 	public get hasErrors(): boolean {
 		if (this.result == null) return false
 		return (this.result?.stats?.hasErrors() || this.result?.stats?.hasWarnings()) ?? false
 	}
+
+	public isScriptError(error: Error): boolean {
+		const stack = error.stack || ''
+		return stack.split('\n').filter(s => s.includes(this.sourceFile)).length > 0
+	}
+
+	liftError?(error: Error): TestScriptError {
+		const stack = error.stack || ''
+
+		const filteredStack = stack.split('\n').filter(s => s.includes(this.sourceFile))
+		let callsite
+		let unmappedStack: string[] = []
+
+		if (filteredStack.length > 0) {
+			callsite = this.sourceUnmapper.unmapCallsite(filteredStack[0])
+			unmappedStack = this.sourceUnmapper.unmapStackNodeStrings(filteredStack)
+		}
+
+		return new TestScriptError(error.message, stack, callsite, unmappedStack, error)
+	}
+
+	// maybeLiftError?(error: Error): Error {}
+
+	// filterAndUnmapStack?(stack: string | Error | undefined): string[] {}
 
 	public get vmScript(): VMScript {
 		if (!this.vmScriptCache) {
@@ -71,12 +105,12 @@ export default class WebpackCompiler implements ITestScript {
 
 	private parsedComments(key: string) {
 		if (!this.parsedCommentsMemo) {
-			let comments = parseComments(this.originalSource)
+			const comments = parseComments(this.originalSource)
 			let description = '',
 				name = ''
 			if (comments.length) {
-				let { description: body } = comments[0]
-				let [line1, ...lines] = body.split('\n')
+				const { description: body } = comments[0]
+				const [line1, ...lines] = body.split('\n')
 				name = line1
 				description = lines
 					.filter((l: string) => l.length)

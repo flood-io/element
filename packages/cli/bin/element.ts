4fc2dc3fd48f3b791902220fd3a92ee6f91fed2f
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2020-01-11 12:19:26 +0800

    chore(cli): adding local import support

--- packages/cli/bin/element.ts
+++ packages/cli/bin/element.ts
@@ -6,9 +6,16 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
 import importLocal from 'import-local'
+import findRoot from 'find-root'
+import { main } from '../src/index'
 
-if (!importLocal(__filename)) {
-	require('element-cli/bin/element')
+if (importLocal(__filename)) {
+	console.log(`Using local version of Element`)
+} else {
+	// require('@flood/element-cli/bin/element')
+	const root = findRoot(__dirname)
+	main(root)
 }
--- packages/cli/index.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-import findRoot from 'find-root'
-const root = findRoot(__dirname)
-import { main } from './src/index'
-main(root)

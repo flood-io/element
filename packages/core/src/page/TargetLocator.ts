0e4074ae62bd9743bb19c2a0176e74805901e5db
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2020-01-15 16:56:13 +0800

    fix(core): frames can now be located by ID

--- packages/core/src/page/TargetLocator.ts
+++ packages/core/src/page/TargetLocator.ts
@@ -10,10 +10,10 @@ export class TargetLocator implements ITargetLocator {
 	constructor(private page: Page, private apply: (frame: Frame | null) => void) {}
 
 	public async activeElement(): Promise<ElementHandle | null> {
-		let jsHandle = await this.page.evaluateHandle(() => document.activeElement || document.body)
+		const jsHandle = await this.page.evaluateHandle(() => document.activeElement || document.body)
 		if (!jsHandle) return null
 
-		let element = jsHandle.asElement()
+		const element = jsHandle.asElement()
 		if (!element) return null
 
 		return new ElementHandle(element).initErrorString()
@@ -45,13 +45,13 @@ export class TargetLocator implements ITargetLocator {
 			return
 		}
 
-		let frames = getFrames(this.page.frames())
+		const frames = getFrames(this.page.frames())
 
 		if (typeof id === 'number') {
 			// Assume frame index
-			let frameElementName = await this.page.evaluate((index: number) => {
+			const frameElementName = await this.page.evaluate((index: number) => {
 				// NOTE typescript lib.dom lacks proper index signature for frames: Window to work
-				let frame = (window as any).frames[Number(index)]
+				const frame = (window as any).frames[Number(index)]
 
 				if (!frame) throw Error(`No frame found at index: ${index}`)
 				return frame.name || frame.id
@@ -64,10 +64,21 @@ export class TargetLocator implements ITargetLocator {
 		} else if (typeof id === 'string') {
 			// Assume id or name attr
 			nextFrame = frames.find(frame => frame.name() === id) || null
+
+			const frameElementName = await this.page.evaluate((id: string) => {
+				// NOTE typescript lib.dom lacks proper index signature for frames: Window to work
+				const frame = Array.from(window.frames).find(frame => frame.frameElement.id === id)
+
+				if (!frame) throw Error(`No frame found with id=${id}`)
+				return frame.name
+			}, id)
+
+			nextFrame = frames.find(frame => frame.name() === frameElementName) || null
+
 			if (!nextFrame) throw new Error(`Could not match frame by name or id: '${id}'`)
 			this.apply(nextFrame)
 		} else if (id instanceof ElementHandle) {
-			let tagName = await id.tagName()
+			const tagName = await id.tagName()
 			if (!tagName || !['FRAME', 'WINDOW', 'IFRAME'].includes(tagName))
 				throw new Error(
 					`ElementHandle supplied to frame() must be a reference to a <frame>, window, or <iframe> element, got <${(tagName &&

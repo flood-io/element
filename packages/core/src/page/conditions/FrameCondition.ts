05a5a0f0804bb5f2e8a199c46ebe144324d40a14
ai@flood.io <65391800+ai-flood@users.noreply.github.com>
2020-05-27 10:02:18 +0700

    fix: through error when running debug on windows (#161)

--- packages/core/src/page/conditions/FrameCondition.ts
+++ packages/core/src/page/conditions/FrameCondition.ts
@@ -1,6 +1,7 @@
 import { Condition, NullableLocatable } from '../Condition'
 import { Frame, Page } from 'puppeteer'
 import { getFrames } from '../../runtime/Browser'
+import { setTimeout } from 'timers'
 
 export class FrameCondition extends Condition {
 	constructor(desc: string, public id: NullableLocatable) {

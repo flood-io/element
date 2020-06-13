06b6237ab06668e977b7ad6096c880e3777aec8e
HieuLeKMS <hieule@kms-technology.com>
2020-06-13 13:46:00 +0700

    feat: select project using project id or name

--- packages/cli/src/cmd/flood/use.ts
+++ packages/cli/src/cmd/flood/use.ts
@@ -3,22 +3,22 @@ import { Argv, Arguments, CommandModule } from 'yargs'
 import { getProjects, setProject, isAuthenticated } from '../../utils/flood'
 
 interface UseArguments extends Arguments {
-	name: string
+	project: string
 }
 
 const cmd: CommandModule = {
-	command: 'use <name>',
-	describe: 'Select a project (by name) to run floods into',
+	command: 'use <project>',
+	describe: 'Select a project (by id or name) to run floods into',
 
 	async handler(args: UseArguments) {
-		const { name } = args
+		const { project } = args
 
 		const projects = await getProjects()
 
 		if (
-			projects.some(project => {
-				if (project.name === name) {
-					setProject({ id: project.id, name: project.name })
+			projects.some(p => {
+				if (p.id === project || p.name === project) {
+					setProject({ id: p.id, name: p.name })
 					return true
 				}
 				return false
@@ -26,13 +26,13 @@ const cmd: CommandModule = {
 		) {
 			console.log(`Using "${name}"`)
 		} else {
-			throw `No project found with name "${name}". Please check and try again`
+			throw `No project found with id or name "${name}". Please check and try again`
 		}
 	},
 	builder(yargs: Argv): Argv {
 		return yargs
-			.positional('name', {
-				describe: 'Project name from Flood',
+			.positional('project', {
+				describe: 'Project id or name from Flood',
 			})
 			.check(() => isAuthenticated())
 	},

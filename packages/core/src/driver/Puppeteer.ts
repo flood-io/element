cf934b248c078d23d34f649f6438f99535a95a65
HieuLeKMS <hieule@kms-technology.com>
2020-07-07 10:27:42 +0700

    chore: revert some changes

--- packages/core/src/driver/Puppeteer.ts
+++ packages/core/src/driver/Puppeteer.ts
@@ -1,4 +1,4 @@
-import { LaunchOptions, Browser, Page, launch as pLaunch } from 'puppeteer'
+import puppeteer, { LaunchOptions, Browser, Page } from 'puppeteer'
 
 import { ChromeVersion } from '../runtime/Settings'
 
@@ -107,7 +107,7 @@ export async function launch(
 
 	options.args.push('--auth-server-whitelist="hostname/domain"')
 
-	const browser = await pLaunch(options)
+	const browser = await puppeteer.launch(options)
 	const page = await browser.newPage()
 
 	return new PuppeteerClient(browser, page)

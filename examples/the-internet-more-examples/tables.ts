9d83236fab2526da33058460534a037f8e94c9b9
Hong La <hong@flood.io>
2020-08-18 08:09:38 +0700

    chore: finish script example at the-internet/tables

--- examples/the-internet-more-examples/tables.ts
+++ examples/the-internet-more-examples/tables.ts
@@ -1,5 +1,4 @@
-import { ElementHandle } from 'puppeteer'
-import { step, Until, By, TestSettings } from '@flood/element'
+import { step, Until, By, TestSettings, Browser, ElementHandle, Locator } from '@flood/element'
 import assert from 'assert'
 
 export const settings: TestSettings = {
@@ -17,43 +16,71 @@ export const settings: TestSettings = {
 const URL = 'https://the-internet.herokuapp.com'
 
 export default () => {
-	step('Test: Go to the Data Tables page', async browser => {
-		await browser.visit(`${URL}/tables`)
-		// Until.titleIs example
-		await browser.wait(Until.titleIs('The Internet'))
-	})
+	step(
+		'Test: Go to the Data Tables page',
+		async (browser: Browser): Promise<void> => {
+			await browser.visit(`${URL}/tables`)
+			// Until.titleIs example
+			await browser.wait(Until.titleIs('The Internet'))
+		},
+	)
 
-	step('Test: findElements example', async browser => {
-		// findElements example
-		const tables = await browser.findElements(By.css('.tablesorter'))
-		const table1Id = await tables[0].getProperty('id')
-		const table2Id = await tables[1].getProperty('id')
-		assert(table1Id === 'table1', 'The id of table 1 must be correct')
-		assert(table2Id === 'table2', 'The id of table 2 must be correct')
-	})
+	step(
+		'Test: findElements example',
+		async (browser: Browser): Promise<void> => {
+			// ElementHandle.findElements(locator: Locator) example
+			const tables: ElementHandle[] = await browser.findElements(By.css('.tablesorter'))
+			const table1Id: string = await tables[0].getProperty('id')
+			const table2Id: string = await tables[1].getProperty('id')
+			assert(table1Id === 'table1', 'The id of table 1 must be correct')
+			assert(table2Id === 'table2', 'The id of table 2 must be correct')
+		},
+	)
 
-	step('Test: Do some sort action with tablesorter', async browser => {
-		const table1El = await browser.findElement(By.css('#table1'))
-		const headers = await table1El.findElements(By.css('.header'))
+	step(
+		'Test: Do some sort action with tablesorter',
+		async (browser: Browser): Promise<void> => {
+			const table: Locator = By.id('table2')
+			const tableEl: ElementHandle = await browser.findElement(table)
+			const headers: ElementHandle[] = await tableEl.findElements(By.css('.header'))
 
-		const rows = await table1El.findElements(By.css('tbody tr'))
-		const cellsOfFirstRow = await rows[0].findElements(By.tagName('td'))
-		const lastName = await cellsOfFirstRow[0].text()
-		const firstActionEdit = await rows[0].findElement(By.partialLinkText('edit'))
-		const firstActionDelete = await rows[0].findElement(By.partialLinkText('delete'))
+			const rows: ElementHandle[] = await tableEl.findElements(By.css('tbody tr'))
+			const cellsOfFirstRow: ElementHandle[] = await rows[0].findElements(By.tagName('td'))
+			const lastName: string = await cellsOfFirstRow[0].text()
+			const firstActionEdit: ElementHandle = await rows[0].findElement(By.partialLinkText('edit'))
+			const firstActionDelete: ElementHandle = await rows[0].findElement(
+				By.partialLinkText('delete'),
+			)
 
-		await firstActionEdit.click()
-		await browser.wait(Until.urlIs(`${URL}/tables#edit`))
-		// await browser.wait(5)
+			await firstActionEdit.click()
+			await browser.wait(Until.urlIs(`${URL}/tables#edit`))
+			await browser.wait(2)
+			await browser.takeScreenshot()
 
-		await firstActionDelete.click()
-		await browser.wait(Until.urlIs(`${URL}/tables#delete`))
-		// await browser.wait(5)
+			await firstActionDelete.click()
+			await browser.wait(Until.urlIs(`${URL}/tables#delete`))
+			await browser.wait(2)
+			await browser.takeScreenshot()
 
-		await headers[0].click()
-		await browser.wait(1)
-		const newFirstCell = await browser.findElement('#table1 tbody tr td')
-		await browser.wait(Until.elementTextDoesNotMatch(newFirstCell, new RegExp(lastName)))
-		await browser.wait(5)
-	})
+			await headers[0].click()
+			await browser.wait(1)
+
+			// By.js() example
+			const newFirstCell: ElementHandle = await browser.findElement(
+				By.js(() => {
+					return document
+						.getElementById('table2')
+						.getElementsByTagName('tbody')[0]
+						.getElementsByClassName('last-name')[0]
+				}),
+			)
+
+			// Until.elementTextDoesNotMatch example
+			await browser.wait(Until.elementTextDoesNotMatch(newFirstCell, new RegExp(lastName)))
+			const newLastName: string = await newFirstCell.text()
+
+			assert(newLastName !== lastName, 'The new last name must be different from the old one.')
+			await browser.wait(2)
+		},
+	)
 }

24acde02d675682dc9be3c3f466a502b0a5a3754
Andy Stanford-Bluntish <andy@bluntish.net>
2020-11-25 20:11:56 +1030

    chore: don't explicitly add import and prettier plugins
    these are added by their individual config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
 	},
 	root: true,
 	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint', 'import', 'prettier'],
+	plugins: ['@typescript-eslint'],
 	extends: [
 		'eslint:recommended',
 		'plugin:@typescript-eslint/eslint-recommended',

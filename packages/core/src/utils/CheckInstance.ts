95823e79223f17b9c36320255c2b8039dc49fe39
Hong La <hongla@kms-technology.com>
2021-01-11 15:04:49 +0700

    fix: fix codacy

--- packages/core/src/utils/CheckInstance.ts
+++ packages/core/src/utils/CheckInstance.ts
@@ -12,20 +12,20 @@ import { BaseLocator } from '../page/Locator'
 import { Point } from '../page/Point'
 import { Locator, ScrollDirection, ElementHandle } from '../page/types'
 
-export const isInstanceOf = (arg: any, conditions: Array<any>) =>
+export const isInstanceOf = (arg: unknown, conditions: Array<any>): boolean =>
 	conditions.some(condition => arg instanceof condition)
 
-export const isAnElementHandle = (arg: any): boolean => isInstanceOf(arg, [TargetElementHandle])
+export const isAnElementHandle = (arg: unknown): boolean => isInstanceOf(arg, [TargetElementHandle])
 
-export const isURLCondition = (arg: any): boolean =>
+export const isURLCondition = (arg: unknown): boolean =>
 	isInstanceOf(arg, [URLCondition, URLNotMatchCondition])
 
-export const isTitleCondition = (arg: any): boolean =>
+export const isTitleCondition = (arg: unknown): boolean =>
 	isInstanceOf(arg, [TitleCondition, TitleNotMatchCondition])
 
-export const isFrameCondition = (arg: any): boolean => isInstanceOf(arg, [FrameCondition])
+export const isFrameCondition = (arg: unknown): boolean => isInstanceOf(arg, [FrameCondition])
 
-export const isElementTextCondition = (arg: any): boolean =>
+export const isElementTextCondition = (arg: unknown): boolean =>
 	isInstanceOf(arg, [ElementTextCondition, ElementTextNotMatchCondition])
 
 export const isLocator = (

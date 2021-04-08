470afca9c4d0315b14620c5298b47f3042dce59b
Andy Stanford-Bluntish <andy@bluntish.net>
2021-04-08 17:48:45 +0930

    chore: update eslint packages and config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,25 @@
 module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+		jest: true,
+	},
 	root: true,
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint', 'prettier', 'import'],
 	extends: [
 		'eslint:recommended',
-		'plugin:@typescript-eslint/eslint-recommended',
 		'plugin:@typescript-eslint/recommended',
 		'prettier/@typescript-eslint',
 		'plugin:import/warnings',
 		'plugin:import/errors',
 		'plugin:import/typescript',
 	],
-
 	rules: {
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-empty-function': 'off',
+		'@typescript-eslint/no-unused-vars': ['warn', {argsIgnorePattern: '^_'}],
 		'@typescript-eslint/no-explicit-any': 'off',
 		'@typescript-eslint/explicit-function-return-type': 'off',
 		'import/no-cycle': 'warn',
